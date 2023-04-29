447d9e3e17bff684340940c0b08f5bb1a221c124
cmz1212 <chua.ming.zhou.1212@gmail.com>
2023-04-29 14:47:50 +0800

    Asthetics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
-import Board from './components/board.js';
+import React, { Component } from "react";
+import "./App.css";
+import Board from "./components/board.js";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Connect Four</h1>
+      <div id="root">
+        <br />
+        <h1>Fourtitude</h1>
+        <br />
         <Board />
       </div>
     );
