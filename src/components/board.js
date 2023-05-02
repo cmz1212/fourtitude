959173d6c3d9e09444453bcd33bb3f70877a06f7
NTU_COSMO <yuhao.huang@ntu.edu.sg>
2023-05-02 17:21:27 +0800

    finished special effects

--- src/components/board.js
+++ src/components/board.js
@@ -40,14 +40,14 @@ class Board extends Component {
     //Traps
     for (let i = 0; i < 3; i++) {
       let x = Math.floor(Math.random() * (c4rows - 1));
-      let y = Math.floor(Math.random() * (c4columns - 1));
+      let y = Math.floor(Math.random() * (c4columns - 2) + 1);
 
       traps.push([x, y]);
     }
 
     while (traps[1].toString() === traps[0].toString()) {
       let x = Math.floor(Math.random() * (c4rows - 1));
-      let y = Math.floor(Math.random() * (c4columns - 1));
+      let y = Math.floor(Math.random() * (c4columns - 2) + 1);
       traps[1].splice(0, 2, x, y);
     }
 
@@ -56,10 +56,9 @@ class Board extends Component {
       traps[2].toString() === traps[1].toString()
     ) {
       let x = Math.floor(Math.random() * (c4rows - 1));
-      let y = Math.floor(Math.random() * (c4columns - 1));
+      let y = Math.floor(Math.random() * (c4columns - 2) + 1);
       traps[2].splice(0, 2, x, y);
     }
-
 
     //Selector
     for (let r = 0; r < 1; r++) {
@@ -72,7 +71,6 @@ class Board extends Component {
 
     //Board
     for (let r = 0; r < c4rows; r++) {
-
       let row = [];
       for (let c = 0; c < c4columns; c++) {
         if (r === traps[0][0] && c === traps[0][1]) {
@@ -107,31 +105,19 @@ class Board extends Component {
       : this.state.player1;
   }
 
-
- 
-   
-
-
-
   play = async (col) => {
-    const { p1Win, p2Win, traps } = this.state;
+    const { p1Win, p2Win, traps, board } = this.state;
 
     if (!this.state.gameOver) {
-      for (let r = 1; r < c4rows + 1; r++) {
-        for (let c = 0; c < c4columns; c++) {
-          const name = r.toString() + c.toString();
-          document.getElementById(name).className = "tile";
-        }
-      }
       let c = Number(col);
-      let board = this.state.board;
-      for (let r = 6; r >= 1; r--) {
+
+      for (let r = 5; r >= 0; r--) {
         if (!board[r][c] || board[r][c] === 3) {
-        new Audio(audio_drop).play();
+          new Audio(audio_drop).play();
           if (board[r][c] === 3) {
             board[r][c] = this.state.currentPlayer;
 
-            this.applyGrass(r, c);
+            this.applyRandom(r, c);
             let t1,
               t2,
               t3 = false;
@@ -163,47 +149,40 @@ class Board extends Component {
             board[r][c] = this.state.currentPlayer;
           }
 
-          const name = r.toString() + c.toString();
-          document.getElementById(name).className = "currenttile";
+          let result = this.checkAll(board);
+          if (result === this.state.player1) {
+            new Audio(audio_win).play();
+            this.setState({ board, gameOver: true, p1Win: p1Win + 1 });
+            alert("Red Wins!");
+          } else if (result === this.state.player2) {
+            new Audio(audio_win).play();
+            this.setState({ board, gameOver: true, p2Win: p2Win + 1 });
+            alert("Yellow wins!");
+          } else if (result === "draw") {
+            new Audio(audio_draw).play();
+            this.setState({ board, gameOver: true });
+            alert("It's a draw!");
+          } else {
+            this.setState({ board, currentPlayer: this.togglePlayer() });
+            let c_name;
 
+            if (this.state.currentPlayer === 2) {
+              c_name = ["player1", "circle"].join(" ");
+            } else if (this.state.currentPlayer === 1) {
+              c_name = ["player2", "circle"].join(" ");
+            }
+
+            document.getElementById("selector" + c.toString()).className =
+              c_name.toString();
+          }
           break;
+        } else {
         }
-      }
-
-      let result = this.checkAll(board);
-      if (result === this.state.player1) {
-
-        new Audio(audio_win).play();
-        this.setState({ board, gameOver: true, p1Win: p1Win + 1 });
-        alert("Red Wins!");
-      } else if (result === this.state.player2) {
-        new Audio(audio_win).play();
-        this.setState({ board, gameOver: true, p2Win: p2Win + 1 });
-        alert("Yellow wins!");
-
-      } else if (result === "draw") {
-        new Audio(audio_draw).play();
-        this.setState({ board, gameOver: true });
-        alert("It's a draw!");
-      } else {
-        this.setState({ board, currentPlayer: this.togglePlayer() });
-        let c_name;
-
-        if (this.state.currentPlayer === 2) {
-          c_name = ["player1", "circle"].join(" ");
-        } else if (this.state.currentPlayer === 1) {
-          c_name = ["player2", "circle"].join(" ");
-        }
-
-        document.getElementById("selector" + c.toString()).className =
-          c_name.toString();
       }
     } else {
       alert("Game over. Please start a new game.");
     }
-
-  }
-
+  };
 
   hoverDisplay(board, c, curr) {
     let audio = new Audio(audio_click);
@@ -215,7 +194,6 @@ class Board extends Component {
     } else if (curr === 2) {
       c_name = ["player2", "circle"].join(" ");
     }
-
 
     document.getElementById("selector" + c.toString()).className =
       c_name.toString();
@@ -339,7 +317,7 @@ class Board extends Component {
   delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
   applyDrop = () => {
     const board = this.state.board;
-    for (let r = 5; r > 0; r--) {
+    for (let r = 4; r >= 0; r--) {
       for (let c = 0; c < 7; c++) {
         if (
           (board[r + 1][c] === null || board[r + 1][c] === 3) &&
@@ -355,6 +333,7 @@ class Board extends Component {
     });
   };
   applyFire = (r, c) => {
+    alert("Special effect triggered: Fire!\nTiles around you are removed!");
     const { board, currentPlayer } = this.state;
     for (let i = c - 1; i <= c + 1; i++) {
       if (board[r + 1][i] !== 3) {
@@ -369,20 +348,22 @@ class Board extends Component {
   };
 
   applyThunder = (r, c) => {
+    alert("Special effect triggered: Thunder!\nTiles in same column are removed!");
     const board = this.state.board;
-    for (let i = c4rows; i > r; i--) {
+    for (let i = c4rows - 1; i > r; i--) {
       this.applyNull(i, c);
     }
     this.setState({
       board,
     });
-
-    this.setState({ board });
   };
 
   applyIce = (r, c) => {
+    alert("Special effect triggered: Ice!\nSpaces around you will be frozen!");
     const board = this.state.board;
-    board[r - 1][c] = 4;
+    if (r >= 1) {
+      board[r - 1][c] = 4;
+    }
     if (board[r][c + 1] === null) {
       board[r][c + 1] = 4;
     }
@@ -392,11 +373,10 @@ class Board extends Component {
     this.setState({
       board,
     });
-
-    this.setState({ board });
   };
 
-  applyGrass = (r, c) => {
+  applyGrowth = (r, c) => {
+    alert("Special effect triggered: Growth!\nYou gain a new tile!");
     const { board, currentPlayer } = this.state;
     let determinant = true;
 
@@ -404,7 +384,7 @@ class Board extends Component {
       const rand_num = Math.floor(Math.random() * 3);
       switch (rand_num) {
         case 0:
-          if (r>1&&(board[r - 1][c] === null || board[r - 1][c] === 3)) {
+          if (r > 1 && (board[r - 1][c] === null || board[r - 1][c] === 3)) {
             board[r - 1][c] = currentPlayer;
             determinant = false;
           }
@@ -424,8 +404,7 @@ class Board extends Component {
         default:
       }
       if (
-        (board[r - 1][c] !== null||r===1) &&
-        board[r - 1][c] !== 3 &&
+        r === 0 &&
         board[r][c + 1] !== null &&
         board[r][c + 1] !== 3 &&
         board[r][c - 1] !== null &&
@@ -441,20 +420,20 @@ class Board extends Component {
     this.setState({ board });
   };
 
-  applyRandom(t1) {
+  applyRandom(r,c) {
     const rand_num = Math.floor(Math.random() * 4);
     switch (rand_num) {
       case 0:
-        this.applyFire(t1[0], t1[1]);
+        this.applyFire(r,c);
         break;
       case 1:
-        this.applyThunder(t1[0], t1[1]);
+        this.applyThunder(r,c);
         break;
       case 2:
-        this.applyIce(t1[0], t1[1]);
+        this.applyIce(r,c);
         break;
       case 3:
-        this.applyGrass(t1[0], t1[1]);
+        this.applyGrass(r,c);
         break;
       default:
     }
@@ -471,7 +450,6 @@ class Board extends Component {
 
     return (
       <div>
-
         <span className="text">
           {gameOver ? gameWinnerMessage : gameTurnMessage}
         </span>
@@ -493,7 +471,6 @@ class Board extends Component {
             ))}
           </tbody>
         </table>
-
 
         <table>
           <thead></thead>
@@ -523,15 +500,7 @@ class Board extends Component {
           }}
         >
           New Game
-
-        
-        <h4>Test buttons</h4>
-        <div display="inline-block">
           
-          <button onClick={() => this.applyRandom(traps[0])}>
-            Apply random{" "}
-          </button>
-
         </div>
       </div>
     );
